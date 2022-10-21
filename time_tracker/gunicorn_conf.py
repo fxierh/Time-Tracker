e2ae82cb5ed6e7aece1a4fd1ada20fbfab37ddf8
fx <feilian1000@126.com>
2022-10-22 00:56:55 +0200

    Re-enable Gunicorn log (to stdout) in local development

--- time_tracker/gunicorn_conf.py
+++ time_tracker/gunicorn_conf.py
@@ -3,16 +3,19 @@ import os
 
 bind = ":8000"
 workers = multiprocessing.cpu_count() * 2 + 1
-# Log with Nginx instead of Gunicorn
-accesslog = None
-errorlog = None
 
 # Production
 if os.environ.get('DEBUG') == 'False':
+    accesslog = None
+    errorlog = None
     preload_app = True
 
 # Local development with or without docker
 else:
+    # Log to stdout
+    accesslog = '-'
+    errorlog = '-'
+
     # Restart workers when code changes. This setting is intended for development.
     reload = True
     reload_extra_files = ["./templates/", "./templates/registration", "./classic_tracker/templates/classic_tracker/"]
